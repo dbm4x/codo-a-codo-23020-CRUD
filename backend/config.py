955d3ba72728be738864cc396a1b1f78ff3294a7
dbm4x <67777721+dbm4x@users.noreply.github.com>
2023-06-27 18:30:57 -0300

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -1,4 +1,4 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:Holanda!123@localhost:3306/flask'
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:elpass@localhost:3306/flask'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = "4C8kum44LxyKWYC8kum4LxyKFyfX4LxyWYLM78sKdXrzbyKWYBjDCFyfX4LxyKWYLM78sKdXrzbyKWYBjDCFyfX"
+    SECRET_KEY = "4C8kum44LxyKWYC8kum4LxyKFyfX4LxbyKWYBjDCFyfX4LxyKWYLM78sKdXrzbyKWYBjDyWYLM78sKdXrzCFyfX"
